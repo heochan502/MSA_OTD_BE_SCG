fc4f8654a7437c57b0f6a553ef9d03dc37a94173
greenhc502 <greenhc502@gmail.com>
2025-09-08 15:05:38 +0900

    gateway 첫커밋

---
+++ src/main/java/com/otd/otd_msa_back_gateway/OtdMsaBackGatewayApplication.java
@@ -0,0 +1,35 @@
+package com.otd.otd_msa_back_gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+/*
+    라우팅 역할
+    - application.yaml
+
+    인증/인가 처리
+    - com.green.scg2.configuration.jwt JWT열어보고 시큐리티 연계용
+    - com.green.scg2.congfiguration.security 시큐리티 관련 세팅
+
+
+    (로그인이 되어 있다면, 즉 JWT 가 넘어온다면)
+    JwtUser는 JWT안에 담겨져있던 claim을 객체로 변환 때 사용
+    UserPrincipal에 JwtUser안에 있는 user_id를 담는다.
+    UserPrincipal이 각 서비스 프로젝트에 전달 된다.
+
+    예외 메시지 처리
+    - com.green.scg2.configuration.exception 패키지 관련
+
+    글로벌 필터
+    - com.green.scg2.configuration.filter 모든 응답처리를 UTF-8로 처리
+ */
+
+@SpringBootApplication
+public class OtdMsaBackGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OtdMsaBackGatewayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/otd/otd_msa_back_gateway/OtdMsaBackGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.otd.otd_msa_back_gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OtdMsaBackGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
