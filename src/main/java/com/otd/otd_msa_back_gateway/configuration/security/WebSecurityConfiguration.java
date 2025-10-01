8bc28630f732f6e5ac2755f381f8f1782cf90886
greenhc502 <greenhc502@gmail.com>
2025-10-01 12:01:39 +0900

    로그인 이슈

--- src/main/java/com/otd/otd_msa_back_gateway/configuration/security/WebSecurityConfiguration.java
+++ src/main/java/com/otd/otd_msa_back_gateway/configuration/security/WebSecurityConfiguration.java
@@ -45,7 +45,7 @@ public class WebSecurityConfiguration  {
         configuration.setAllowCredentials(true);
 
         if(Arrays.asList(activeProfiles).contains("prod")) {
-            configuration.addAllowedOrigin("https://greenart.n-e.kr , http://localhost:5173, http://localhost:5174 ");
+            configuration.addAllowedOrigin("https://greenart.n-e.kr ");
         } else {
             configuration.setAllowedOriginPatterns(List.of("*"));
         }
